dd23566582e68445a5a6d0ada43dda89a107d9ab
Corey Reed <coreyr101@gmail.com>
2023-06-08 22:27:11 -0400

    update

--- bootstrap-starter/webpack.config.js
+++ bootstrap-starter/webpack.config.js
@@ -1,21 +1,46 @@
 "use strict";
 
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/main.js',
+  mode: "development",
+  entry: "./src/js/main.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
     port: 8080,
-    hot: true
+    hot: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' })
-  ]
-}
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [require("autoprefixer")],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
